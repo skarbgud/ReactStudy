03cec108fe308b0d7ee3b94ed8d7d25ef7a8d371
Nam <sjarbgud@gmail.com>
2020-08-17 23:23:46 +0900

    React - 15.3 key

--- src/App.js
+++ src/App.js
@@ -35,7 +35,12 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:"World Wide Web!"}
+      subject:{title:'WEB', sub:"World Wide Web!"},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
     }
   }
   render() {
@@ -45,7 +50,7 @@ class App extends Component {
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
