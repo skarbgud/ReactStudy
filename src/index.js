dafc2500ef42e6987a1e07c5a9141b9b35567a5e
Nam <sjarbgud@gmail.com>
2020-08-18 21:49:33 +0900

    React - 16.1. 이벤트 state props 그리고 render 함수

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import './index.css';
 import App from './App';  //App.js파일에서 임포트함
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* 리액트를 통한 사용자정의 태그 (컴포넌트) */}
-    <App />     
-  </React.StrictMode>,
-  document.getElementById('root') //index.html에서 있는 id가 root에 렌더링됨 
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
