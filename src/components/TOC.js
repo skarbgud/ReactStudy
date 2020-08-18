6c177713092f6453d328c33bb53c7b5beae2e156
Nam <sjarbgud@gmail.com>
2020-08-19 00:20:11 +0900

    React - 17.3. 컴포넌트 이벤트 만들기

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,10 +11,12 @@ class TOC extends Component {
                 <li key={data[i].id}>
                     <a 
                         href={"/content/"+data[i].id}
-                        onClick={function(e){
+                        // data-id={data[i].id}
+                        onClick={function(id, e){
                             e.preventDefault();
-                            this.props.onChangePage();
-                        }.bind(this)}
+                            // this.props.onChangePage(e.target.dataset.id);
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
                     >{data[i].title}</a>
                 </li>);
             i = i + 1;
