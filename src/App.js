b2e51716ef668193a8d0a4ef55fa4a2faf109d69
Nam <sjarbgud@gmail.com>
2020-08-19 22:59:25 +0900

    React - 19.3. create 구현 : mode 전환 기능

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 
@@ -49,10 +50,11 @@ class App extends Component {
   }
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
       var i = 0;
       while(i < this.state.contents.length){
@@ -64,6 +66,9 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent title={_title} desc={_desc}></CreateContent>
     }
     return (
       <div className="App">
@@ -89,7 +94,7 @@ class App extends Component {
             mode:_mode,
           });
         }.bind(this)}></Control>
-        <Content title={_title} desc={_desc}></Content>
+        {_article}
       </div>
     );
   }
