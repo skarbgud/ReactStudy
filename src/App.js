c1899e8733e3c809198b41b168f9c9baa180cc2e
Nam <sjarbgud@gmail.com>
2020-08-19 23:29:22 +0900

    React - 19.5. create 구현 : onSubmit 이벤트

--- src/App.js
+++ src/App.js
@@ -68,7 +68,10 @@ class App extends Component {
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'create'){
-      _article = <CreateContent title={_title} desc={_desc}></CreateContent>
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        // add content to this.state.contents
+        console.log(_title,_desc);
+      }.bind(this)}></CreateContent>
     }
     return (
       <div className="App">
