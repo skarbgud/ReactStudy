0010cb9035f76bb693175c0312e696859902e8b5
Nam <sjarbgud@gmail.com>
2020-08-21 00:03:50 +0900

    React - 19.7. create 구현 : shouldComponentUpdate

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 
 class TOC extends Component {
+    shouldComponentUpdate(newProps, newState){
+        console.log('======>TOC render shouldComponentUpdate'
+            ,newProps.data  //바뀐 값
+            ,this.props.data //현재 값
+        );
+        // shouldComponentUpdate 함수는 호출 되었지만 render 함수는 호출 X
+        if(this.props.data === newProps.data){
+            return false;
+        }
+        // push를 하면 원본의 값을 수정하게 되므로 바뀐 값 감지 못한다.
+        return true;   //true -> render 호출 , false -> render 호출 X
+    }
     render() {
-        console.log('TOC render');
+        console.log('======>TOC render');
+        // render 함수를 다시 호출 할지 말지 결정 할 수 있게한다.
         var lists = [];
         var data = this.props.data;
         var i = 0;
