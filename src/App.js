123fdf1f6659c300a989bf1b19ea64ed9403fc8d
Nam <sjarbgud@gmail.com>
2020-08-22 04:22:39 +0900

    React - 20.3. update 구현 : state 변경

--- src/App.js
+++ src/App.js
@@ -59,23 +59,29 @@ class App extends Component {
             {id:this.max_content_id, title:_title, desc:_desc}
         )
         this.setState({
-          contents:newContents
+          contents:newContents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
         console.log(_title,_desc);
       }.bind(this)}></CreateContent>
     } else if(this.state.mode === 'update'){
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
-        // add content to this.state.contents
-        this.max_content_id = this.max_content_id + 1;
-       
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        )
-        this.setState({
-          contents:_contents
-        });
-        console.log(_title,_desc);
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc){
+          var _contents = Array.from(this.state.contents);  //배열 복사
+          var i = 0;
+          while(i< _contents.length){
+            if(_contents[i].id === _id){
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
       }.bind(this)}></UpdateContent>
     }
     return _article;
